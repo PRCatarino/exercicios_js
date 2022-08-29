0d92d3453e73fa898d332911137f6643e4b0ed6b
PRCatarino <paulo061104@gmail.com>
2022-08-29 20:51:44 -0300

    aula-2

---
+++ aula-2/aula-2.js
@@ -0,0 +1,19 @@
+//A
+{
+let repetidos = 15 * 7
+   let ganho = 7 * 30
+   let colagem =  ganho - repetidos  
+
+   console.log(colagem)
+}
+//B
+{
+   let caixaGuarda = 12
+   let caixas = caixaGuarda * 4
+
+   console.log(caixas)
+}
+//C
+{
+  
+}
