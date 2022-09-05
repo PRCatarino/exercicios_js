e4bf695a089203d0c81c61a04cb1f1ce91dc2eb7
PRCatarino <paulo061104@gmail.com>
2022-09-05 18:19:33 -0300

    aula-6

--- aula-4/aula-4.js
+++ aula-4/aula-4.js
@@ -1,15 +1,26 @@
-let guardaLetra = prompt('Digite uma letra do alfabeto:').toLowerCase();
+// {
+//    let recebeLetra = prompt('Digite uma letra do alfabeto:').toLowerCase();
+//    let letraValida;
+//    letraValida = !isNaN(recebeLetra) ? false : true;
 
+//    if(letraValida && recebeLetra.length == 1){
+//       if(recebeLetra=='a' || recebeLetra=="e" || recebeLetra=="i"|| guardaLetra == 'o' || guardaLetra == 'u'){
+//          alert('A letra digitada é uma vogal');
+//       }else{
+//          alert('A letra digitada é uma consoante');
+//       }
+//    }else{
+//       alert("Você deve digitar uma letra")
+//    }
+// }
+{
+   let recebeLetra = prompt('Digite uma letra do alfabeto').toLowerCase();
+   let letraValida = !isNaN(recebeLetra) ? false : true;
+   console.log(letraValida)
 
-if(guardaLetra == 1 || guardaLetra == 2 || guardaLetra == 3 || guardaLetra == 4 || guardaLetra == 5 || guardaLetra == 6 || guardaLetra == 7 || guardaLetra == 8 || guardaLetra == 9 || guardaLetra == 0 ){
-   alert('Deve digitar uma letra do alfabeto para obter o resultado')
-}else{
-   if(guardaLetra =='a' || guardaLetra == 'e' || guardaLetra == 'i' || guardaLetra == 'o' || guardaLetra == 'u'){
-      alert("Letra digitada é uma vogal")
-   }else{
-      alert("Letra digitada é uma cosoante")
+   switch (recebeLetra){
+      case 'a' || 'e' || 'i' || 'o' || 'u':
+         alert('É uma vogal');
+         break;
    }
 }
-
-
-
