e07878edaa00ffc356085a21d5b0ab4ad3f07ffd
PRCatarino <paulo061104@gmail.com>
2022-09-05 21:54:30 -0300

    aula 6

---
+++ aula-6/aula-6.js
@@ -0,0 +1,13 @@
+let recebeAnos = prompt("quantos Ano");
+
+let reciclagemFeita = recebeAnos / 2
+
+if( recebeAnos <= 1){
+    console.log('Necessário ser um numero maior que 1')
+
+    }else if(recebeAnos / 2){
+    console.log('já fizeram', reciclagemFeita)
+
+    }else{
+    console.log('digitar um numero');
+}
---
+++ aula-7/aula-.js
@@ -0,0 +1,8 @@
+var pilotos = prompt('digite pilotos').split(',');
+console.log(pilotos.splice(0, 3));
+
+// for(var i = 0; i < pilotos.length; i++){
+//     console.log(pilotos[i]);
+//     let vencedores = pilotos.splice(0, 3)
+// }
+
