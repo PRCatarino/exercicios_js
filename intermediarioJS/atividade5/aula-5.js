98eae12c7abdab466dc9966b058c26fe527bee16
PRCatarino <paulo061104@gmail.com>
2022-09-26 14:29:54 -0300

    formulario

--- intermediarioJS/atividade5/aula-5.js
+++ intermediarioJS/atividade5/aula-5.js
@@ -1,12 +1,12 @@
-// let recebeCPF = prompt('Digite cpfs');
-// let listaCpf = recebeCPF.split(',')
-// console.log(listaCpf);
+let recebeCPF = prompt('Digite cpfs');
+let listaCpf = recebeCPF.split(',')
+console.log(listaCpf);
 
-// function checkCpfs(){
-//     for(brakeList of listaCpf){
-//         if(brakeList.length == 11){
-//             console.log('true');
-//         }
-//     }
-// }
-// console.log(checkCpfs(listaCpf));
+function checkCpfs(){
+    for(brakeList of listaCpf){
+        if(brakeList.length == 11){
+            console.log('true');
+        }
+    }
+}
+console.log(checkCpfs(listaCpf));
