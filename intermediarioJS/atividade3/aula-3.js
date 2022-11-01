8eeb41558a92bbc04b3e9b3bc2e050fd898277bc
PauloRoberto <77987835+PRCatarino@users.noreply.github.com>
2022-11-01 20:59:41 -0300

    Delete intermediarioJS directory

--- intermediarioJS/atividade3/aula-3.js
+++
@@ -1,9 +0,0 @@
-let apartamento = {
-    quarto : ['Guarda-Roupa', 'Cama', 'Computador', 'cômoda','Televisão'],
-    sala : ['Televisão','Estante','Sofá', 'Mesa'],
-    cozinha : ['Geladeira','Fogão','Sugar','Pia','Armários'],
-    banheiro : ['Vaso','Choveiro','Pia','Espelho']
-};
-
-console.log('Apartamento',apartamento);
-console.log('Quarto',apartamento.quarto);
