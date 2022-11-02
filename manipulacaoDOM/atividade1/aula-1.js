742faeb7c32ae213d7d96527ab63f45c65566328
PauloRoberto <77987835+PRCatarino@users.noreply.github.com>
2022-11-01 21:00:23 -0300

    Delete manipulacaoDOM directory

--- manipulacaoDOM/atividade1/aula-1.js
+++
@@ -1,8 +0,0 @@
-for(let i = 0; i <= 4; i++){console.log(document.getElementsByTagName('p')[i].innerHTML)}
-
-
-
-
-
-
-
--- manipulacaoDOM/atividade2/aula-2.js
+++
@@ -1,6 +0,0 @@
-let guardaBody = document.querySelector('body');
-let menor = false
-if (guardaBody.childElementCount >= 5){
-    menor = true
-}
-console.log(menor)
